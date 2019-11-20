7646f25312b4d4240dad914d44533460ff5f58e3
parunkov <parunkov@gmail.com>
2019-11-20 09:44:00 +0500

    Создал gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -7,15 +7,36 @@ const PATHS = {
 };
 
 module.exports = {
-    entry: PATHS.source + '/index.js',
+    mode: 'development',
+    entry: {
+        'index': PATHS.source + '/pages/index/index.js',
+        'blog': PATHS.source + '/pages/blog/blog.js'
+    },
     output: {
         path: PATHS.build,
         filename: '[name].js'
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Webpack app'
+            filename: 'index.html',
+            chunks: ['index'],
+            template: PATHS.source + '/pages/index/index.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'blog.html',
+            chunks: ['blog'],
+            template: PATHS.source + '/pages/blog/blog.pug'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader',
+                options: {
+                    pretty: true
+                }
+            }
+        ]
+    }
 };
-
